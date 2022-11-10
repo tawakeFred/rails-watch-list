b15c777b52bc307f448d024757f2ab48b2d9c693
tawakeFred <fred@twk.mn>
2022-11-10 13:26:00 +0900

    Fixed models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,7 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, presence: true, length: { minimum: 6 }
-  validates :list_id, presence: true, uniqueness: {
-    scope: [ :movie_id, :list_id ]
+  validates :list, presence: true, uniqueness: {
+    scope: [ :movie, :list ]
   }
 end
