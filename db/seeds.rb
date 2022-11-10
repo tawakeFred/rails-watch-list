6b696bb2a3e334db2f6d9ec1eee291742e55554a
tawakeFred <fred@twk.mn>
2022-11-10 12:24:32 +0900

    Models and seed fixex

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "- Removing all movies!"
+Movie.destroy_all
+# List.destroy_all
+
+# the Le Wagon copy of the API
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+response = JSON.parse(URI.open(url).read)
+
+response['results'].each do |movie_hash|
+  movie_title = movie_hash['title']
+  movie_overview = movie_hash['overview']
+  movie_poster = "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path']
+  movie_rating = movie_hash['vote_average']
+
+  # create an instance with the hash
+  Movie.create!(title: movie_title, overview: movie_overview, poster_url: movie_poster, rating: movie_rating)
+end
+puts " - Added #{Movie.count} movies"
